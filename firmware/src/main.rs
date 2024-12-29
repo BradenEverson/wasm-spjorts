976efbd1d8d260cf25846992d62da1f49c9ece7d
Braden Everson <bradeneverson@gmail.com>
2024-12-28 21:01:22 -0600

    Create message send channels

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,9 +1,11 @@
 //! Main firmware driver for a controller, reading rotational data and button press events from the
 //! Pi and transmitting this information to the game server over web sockets
 
-use std::{fs::File, io::Read, time::Duration};
-
+use futures_util::{SinkExt, StreamExt};
 use rppal::gpio::{Gpio, Trigger};
+use server::control::{msg::WsMessage, ControllerMessage};
+use std::{fs::File, io::Read, sync::mpsc::channel, time::Duration};
+use tokio_tungstenite::connect_async;
 
 /// A Button's pin
 pub const BUTTON_A_PIN: u8 = 5;
@@ -13,8 +15,22 @@ pub const BUTTON_B_PIN: u8 = 6;
 #[tokio::main]
 async fn main() {
     let id = read_id();
+    let (write_action, read_action) = channel();
 
     // Connect to server
+    let (ws, _) = connect_async("ws://192.168.10.137:7878")
+        .await
+        .expect("Connect to ws");
+
+    let (mut write, _) = ws.split();
+    write
+        .send(
+            WsMessage::Controller(id)
+                .to_ws_message()
+                .expect("Convert to ws message"),
+        )
+        .await
+        .unwrap();
 
     // Set up peripherals
     let gpio = Gpio::new().expect("Initialize GPIO");
@@ -28,28 +44,68 @@ async fn main() {
         .expect("Get GPIO pin for B button")
         .into_input_pulldown();
 
+    let channel_a = write_action.clone();
     button_a
         .set_async_interrupt(
             Trigger::FallingEdge,
             Some(Duration::from_millis(50)),
-            move |event| {
-                println!("Button A Event: {event:?}");
+            move |_| {
+                channel_a
+                    .send(ControllerMessage::ButtonPressA)
+                    .expect("Send button press A");
             },
         )
         .expect("Set interupt");
 
+    let channel_b = write_action.clone();
     button_b
         .set_async_interrupt(
             Trigger::FallingEdge,
             Some(Duration::from_millis(50)),
-            move |event| {
-                println!("Button B Event: {event:?}");
+            move |_| {
+                channel_b
+                    .send(ControllerMessage::ButtonPressB)
+                    .expect("Send button press B");
             },
         )
         .expect("Set interupt");
 
-    // Read gyroscope information and send it over to websocket
-    loop {}
+    // Read gyroscope information and send it over to web socket
+    while let Ok(msg) = read_action.recv() {
+        match msg {
+            ControllerMessage::ButtonPressA => {
+                write
+                    .send(
+                        ControllerMessage::ButtonPressA
+                            .to_ws_message()
+                            .expect("Convert to ws message"),
+                    )
+                    .await
+                    .unwrap();
+            }
+            ControllerMessage::ButtonPressB => {
+                write
+                    .send(
+                        ControllerMessage::ButtonPressB
+                            .to_ws_message()
+                            .expect("Convert to ws message"),
+                    )
+                    .await
+                    .unwrap();
+            }
+            ControllerMessage::AngleInfo(pitch, roll, yaw) => {
+                write
+                    .send(
+                        ControllerMessage::AngleInfo(pitch, yaw, roll)
+                            .to_ws_message()
+                            .unwrap(),
+                    )
+                    .await
+                    .unwrap();
+            }
+            _ => {}
+        }
+    }
 }
 
 /// Gets the controller ID from the configuration files
